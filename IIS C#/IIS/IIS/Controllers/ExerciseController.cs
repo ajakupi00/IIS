adf46a45f8ab63dfccfa60c2bb4dfa9a5135cfb8
Arjan Jakupi <ajakupi@racunarstvo.hr>
2023-03-25 14:39:41 +0100

    xsd

---
+++ IIS C#/IIS/IIS/Controllers/ExerciseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace IIS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExerciseController : ControllerBase
+    {
+        [HttpPost]
+        public void XSD(string responseXML)
+        {
+            try
+            {
+                XDocument xml = XDocument.Parse(responseXML);
+
+                XmlSchemaSet xmlSchemaSet = new XmlSchemaSet();
+                xmlSchemaSet.Add("", "./Validators/ExerciseXSD.xsd");
+
+                xml.Validate(xmlSchemaSet, (o, e) =>
+                {
+                    Console.WriteLine("Valid");
+                });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
